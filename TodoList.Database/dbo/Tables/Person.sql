9a7fab628f18a90d1e5fac2f357e188fb9cbaa56
Leo Tracchia <ltracchia@gmail.com>
2023-06-06 17:02:44 -0400

    new database objects for identity framework

---
+++ TodoList.Database/dbo/Tables/Person.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Person] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName] VARCHAR (50)   NOT NULL,
+    [LastName]  VARCHAR (50)   NOT NULL,
+    [FkUserId]  NVARCHAR (450) NOT NULL,
+    CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Person_AspNetUsers] FOREIGN KEY ([FkUserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
