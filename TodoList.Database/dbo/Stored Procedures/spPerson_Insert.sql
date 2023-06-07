8b9c4c1eac5c7d0b488fdfaafa56985f547043c6
Leo Tracchia <ltracchia@gmail.com>
2023-06-06 20:35:44 -0400

    modiifed database objects for new Person table

---
+++ TodoList.Database/dbo/Stored Procedures/spPerson_Insert.sql
@@ -0,0 +1,19 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE spPerson_Insert
+	@FirstName varchar(50),
+	@LastName varchar(50),
+	@UserId nvarchar(250)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	insert into Person (FirstName, LastName, FkUserId, DateCreated) values (@FirstName, @LastName, @UserId, getdate());
+
+END
--- TodoList.Database/dbo/Tables/Person.sql
+++ TodoList.Database/dbo/Tables/Person.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[Person] (
-    [Id]        INT            IDENTITY (1, 1) NOT NULL,
-    [FirstName] VARCHAR (50)   NOT NULL,
-    [LastName]  VARCHAR (50)   NOT NULL,
-    [FkUserId]  NVARCHAR (450) NOT NULL,
-    CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Person_AspNetUsers] FOREIGN KEY ([FkUserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]    VARCHAR (50)   NOT NULL,
+    [LastName]     VARCHAR (50)   NOT NULL,
+    [FkUserId]     NVARCHAR (450) NOT NULL,
+    [DateCreated]  DATETIME       NULL,
+    [DateModified] DATETIME       NULL,
+    CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
